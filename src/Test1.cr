02ae062eb01851eb59148ec6910da43bcd61b2b9
Kai Henningsen <kai.extern+git@gmail.com>
2018-08-04 15:12:28 +0200

    Protobuf etc

--- src/Test1.cr
+++ src/Test1.cr
@@ -1,6 +1,10 @@
 require "./Test1/*"
+require "protobuf"
 
 # TODO: Write documentation for `Test1`
 module Test1
   # TODO: Put your code here
+  struct MyMessage
+    include Protobuf::Message
+  end
 end
